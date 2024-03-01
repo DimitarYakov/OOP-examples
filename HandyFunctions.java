b8f85bb35341aad9d879da83ba58220ca183472c
DimitarYakov <34747940+DimitarYakov@users.noreply.github.com>
2024-03-01 18:50:00 +0200

    Useful functions in java

---
+++ HandyFunctions.java
@@ -0,0 +1,69 @@
+// CONVERT RAW STR TO HEX STR
+    public static String toHex(byte[] bytes) {
+        BigInteger bi = new BigInteger(1, bytes);
+        return String.format("%0" + (bytes.length << 1) + "X", bi);
+    }
+    
+    private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
+    public static byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                    + Character.digit(s.charAt(i + 1), 16));
+        }
+        return data;
+    }
+    
+    //CONVERT HEX STR TO ASCII
+    public String fromHex(String hex) throws UnsupportedEncodingException {
+        hex = hex.replaceAll("^(00)+", "");
+        byte[] bytes = new byte[hex.length() / 2];
+        for (int i = 0; i < hex.length(); i += 2) {
+            bytes[i / 2] = (byte) ((Character.digit(hex.charAt(i), 16) << 4) + Character.digit(hex.charAt(i + 1), 16));
+        }
+        return new String(bytes);
+    }
+    
+    public static String xorHex(String a, String b) {
+        int j = 0;
+        char[] chars = new char[a.length()];
+        for (int i = 0; i < chars.length; i++) {
+            chars[i] = toHex(fromHex(a.charAt(i)) ^ fromHex(b.charAt(j)));
+            j+=1;
+            if(j == 64){
+                j = 0;
+            }
+        }
+        return new String(chars);
+    }
+
+    private static int fromHex(char c) {
+        if (c >= '0' && c <= '9') {
+            return c - '0';
+        }
+        if (c >= 'A' && c <= 'F') {
+            return c - 'A' + 10;
+        }
+        if (c >= 'a' && c <= 'f') {
+            return c - 'a' + 10;
+        }
+        throw new IllegalArgumentException();
+    }
+
+    private static char toHex(int nybble) {
+        if (nybble < 0 || nybble > 15) {
+            throw new IllegalArgumentException();
+        }
+        return "0123456789ABCDEF".charAt(nybble);
+    }
